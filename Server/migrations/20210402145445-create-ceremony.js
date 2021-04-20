d9772b1cf398d5cfba9cb6dd396d3c0b1e9ffdf9
alexispp <alexis.petroccelli@gmail.com>
2021-04-20 00:21:21 -0300

    fix database issues

--- Server/migrations/20210402145445-create-ceremony.js
+++ Server/migrations/20210402145445-create-ceremony.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Ceremonies', {
+    await queryInterface.createTable('ceremonies', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Ceremonies');
+    await queryInterface.dropTable('ceremonies');
   }
 };
