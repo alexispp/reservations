c8f70e7c173ae4b267177ee8b2e645af3ae4ab66
alexis <alexispetroccelli@gmail.com>
2021-08-04 23:52:02 -0300

    .

--- config/config.js
+++ config/config.js
@@ -1,32 +1,32 @@
-require('dotenv').config()
+require("dotenv").config();
 module.exports = {
-  development: {
-    username: process.env.DEV_DB_USERNAME,
-    password: process.env.DEV_DB_PASSWORD,
-    database: process.env.DEV_DB_DATANASE,
-    host: process.env.DEV_DB_HOST,
-    port: process.env.DEV_DB_PORT,
-    dialect: "postgres",
-  },
-  test: {
-    username: "",
-    password: "",
-    database: "",
-    host: "",
-    dialect: "postgres",
-  },
-  production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_DATANASE,
-    host: process.env.PROD_DB_HOST,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+    development: {
+        username: process.env.DEV_DB_USERNAME,
+        password: process.env.DEV_DB_PASSWORD,
+        database: process.env.DEV_DB_DATANASE,
+        host: process.env.DEV_DB_HOST,
+        port: process.env.DEV_DB_PORT,
+        dialect: "postgres"
     },
-    use_env_variable: "DATABASE_URL"
-  },
+    test: {
+        username: "",
+        password: "",
+        database: "",
+        host: "",
+        dialect: "postgres"
+    },
+    production: {
+        username: process.env.PROD_DB_USERNAME,
+        password: process.env.PROD_DB_PASSWORD,
+        database: process.env.PROD_DB_DATANASE,
+        host: process.env.PROD_DB_HOST,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        use_env_variable: "DATABASE_URL"
+    }
 };
